0e0d1a662abade229d2841b6d82bde5b8cfeeaeb
levdalba <levanidalbashvili00@gmail.com>
2023-10-31 07:26:39 +0700

    Missing Number solved

--- icpc.py
+++ icpc.py
@@ -1,13 +1,8 @@
-num = int(input())
-list = []
+n = int(input())
+a = set(map(int, input().split()))
 
-while num != 1:
-    list.append(num)
-    if num % 2 == 0:
-        num = num // 2
-    else:
-        num = num * 3 + 1
-
-list.append(1)
-
-print(" ".join(map(str, list)))
+# for i in range(1, n + 1):
+#     if i not in a:
+#         print(i)
+#         break
+print(a)
